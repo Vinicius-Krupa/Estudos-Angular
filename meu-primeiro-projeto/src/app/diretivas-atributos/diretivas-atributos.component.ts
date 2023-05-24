2274ec582012d25ba690e98dd09d6c7a95c38762
vsoa <vinicius.oliveira1@gft.com>
2023-05-24 14:24:50 -0300

    ng-template

--- meu-primeiro-projeto/src/app/diretivas-atributos/diretivas-atributos.component.ts
+++ meu-primeiro-projeto/src/app/diretivas-atributos/diretivas-atributos.component.ts
@@ -13,7 +13,7 @@ export class DiretivasAtributosComponent implements OnInit {
   public backgroundColor: string = "red";
 
   public nome: string = "";
-  public list: Array<{nome: string}> = [];
+  public list: Array<{nome: string}> = [{ nome: "Vinicius Krupa" }];
 
   constructor() { }
 
